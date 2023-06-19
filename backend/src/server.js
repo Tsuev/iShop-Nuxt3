8f197acea7a1f34baadaf88ae1ae0e076cf1e819
Superior5 <superiorunknown5@gmail.com>
2023-06-19 14:16:20 +0300

    asdf

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(userRouter)
+app.use("/api",userRouter)
 
 
 app.use(errorHandler);
