1b52707ebd21e1136cdd036e69539c4198f045b0
Superior5 <superiorunknown5@gmail.com>
2023-06-19 14:07:57 +0300

    db

--- backend/src/db.js
+++ backend/src/db.js
@@ -10,6 +10,6 @@ export default new Sequelize(
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: `${process.env.DB_PORT}`,
     }
 )
--- backend/src/middleware/authMiddlware.js
+++ backend/src/middleware/authMiddlware.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 
+
 export default function (req, res, next) {
     if (req.method === 'OPTIONS') {
         next();
